ddbb2ac62e3e2ac2217795288ddfed3f6a56b9b6
shivCan99 <107194536+shivCan99@users.noreply.github.com>
2023-08-16 20:41:43 -0400

    contact-api2

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
-
+import { FormsModule, NgForm } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  Validators,
+  ReactiveFormsModule,
+} from '@angular/forms';
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent {
+  email: any; 
+  onSubmit(form: NgForm) {
+    console.log(form);
+    form.reset();
+  }
+
+  groceryForm = new FormGroup({
+    name: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    email: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    //email: new FormControl<number | null>(null, Validators.required),
+  });
+
 
 }
